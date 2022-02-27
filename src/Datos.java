009f78e289bfa2171384c35062d12d022afe4325
Lennin <lennin5412@gmail.com>
2022-02-27 13:58:18 -0600

    subida

---
+++ src/Datos.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenni
+ */
+public class Datos {
+    private String nombre;
+    private String apellido;
+    private int carnet;
+    
+    public Datos(String nombre,String apellido, int carnet){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.carnet=carnet;
+    }
+    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the carnet
+     */
+    public int getCarnet() {
+        return carnet;
+    }
+
+    /**
+     * @param carnet the carnet to set
+     */
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+    
+}
